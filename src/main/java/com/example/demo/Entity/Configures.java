7c7ac2729516809e0975bbded672bbbb2cdf7c80
iOLO <516502968@qq.com>
2018-04-20 08:55:42 +0800

    添加了注入构造方法，并且确定注入的顺序，在构造方法里，添加mapper的注入对象，因为是Autowired单例的模式，所以可以复用

--- src/main/java/com/example/demo/Entity/Configures.java
+++ src/main/java/com/example/demo/Entity/Configures.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +21,7 @@ public class Configures {
     private static final Logger logger = LoggerFactory.getLogger(Configures.class);
 
     public static Map<String, String> map = new HashMap<>();
+    public static Map<String, String> map1 = new HashMap<>();
 
     @Autowired
     private ConfiguresMapper configuresMapper;
@@ -27,6 +30,22 @@ public class Configures {
     public String server;
     public String name;
     public String value;
+
+    public Configures() {
+    }
+
+    @Autowired
+    public Configures(ConfiguresMapper configuresMapper1) {
+        Map<String, String> mapC = new HashMap<>();
+        List<Configures> list = configuresMapper1.getAll();
+        if (list != null && list.size() != 0) {
+            list.forEach(obj -> mapC.put(obj.name, obj.value));
+            this.map1 = mapC;
+            logger.info("ConfiguresBean is Success");
+        } else {
+            logger.error("ConfiguresBean error : Map is null !!!");
+        }
+    }
 
     @Bean
     public Configures getConfiguresBean() {
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.Entity.Configures;
+import com.example.demo.mapper.ConfiguresMapper;
 import com.example.demo.util.Redis.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -18,6 +21,8 @@ public class TestController{
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private RedisService redisService;
+    @Autowired
+    private ConfiguresMapper configuresMapper;
 
     @RequestMapping(value = {"/", ""})
     public String hellTask() {
@@ -26,6 +31,9 @@ public class TestController{
 
         logger.info(Configures.map.get("spring.redis.host"));
         logger.info(Configures.map.get("spring.redis.port"));
+        logger.info("map1:----"+Configures.map1.get("spring.redis.port"));
+
+        List<Configures> list = configuresMapper.getListByServer("server1");
 
         return "value : "+ value;
     }
